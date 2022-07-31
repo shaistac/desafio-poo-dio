ab4d3b5acbd66a0c5156750faa6046a0fbbd3b6d
shaistac <shaistacamara@gmail.com>
2022-07-31 10:22:06 -0300

    add class Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        //nem = instanciando o objeto curso
+        //a classe Curso é a planta da casa e o objetvo curso1 é a casa construída
+        Curso curso1 = new Curso();
+        curso1.setTitulo("BootCamp Code Girls");
+        curso1.setDescricao("BootCamp sobre Java");
+        curso1.setCargaHoraria(62);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("BootCamp Impulso JavaScript Evolution");
+        curso2.setDescricao("BootCamp sobre Front-End");
+        curso2.setCargaHoraria(104);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria X");
+        mentoria.setDescricao("Realizada por Y");
+        mentoria.setData(LocalDate.now()); //vai atribuir a data de criação
+
+        //imprimindo
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
